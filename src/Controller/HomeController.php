c9ad1e533b49731b75ac202b5b3b66f1ed6e25c2
foforato <arhim45@gmail.com>
2021-01-08 22:47:06 +0100

    jbhjd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\CustomerReview;
 use App\Entity\GoodDeal;
+use App\Repository\CityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HomeController extends AbstractController
 {
@@ -17,14 +19,21 @@ class HomeController extends AbstractController
      * @return Response
      */
 
-    public function index(): Response
-    {;
+    public function index(CityRepository $cityRepository, SerializerInterface $serializer): Response
+    {
         $good_deal = $this-> getDoctrine()
             ->getRepository(GoodDeal::class)
             ->findAll();
-
+        $cities = $cityRepository->findAll();
+        $NbCities = count($cities);
+        $map= array();
+        for($i = 0; $i < $NbCities; $i++) {
+            $map[] = '"' . $cities[$i]->getName() . '"' . "," . $cities[$i]->getLongitude() . ",". $cities[$i]->getLatitude();
+        }
+        $maps = $serializer->serialize($map, 'json');
         return $this->render('home/index.html.twig', [
             'good_deal' => $good_deal,
+            'maps' => $maps
         ]);
     }
 }
